da530690a923c1d9b9f77c28c849b631c931e3a6
Christian Wacker <christianwwacker@gmail.com>
2020-01-06 22:26:01 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,12 @@
 function basicTeenager(age) {
-  if (age > 13 && age < 19) 
+  if (age > 13 && age < 19)
     return "You are a teenager!"
 }
 
 function teenager(age) {
-
+  if (age > 13 && age < 19)
+    return "You are a teenager!"
+  } else if (age < 13 && age > 19) {
 }
 
 function ageChecker(age) {
